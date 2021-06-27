eec4defdffbf6ca56455548bb76c3646acfce8a7
SUB_ACCOUNT <kensuke.mitsuzawa@eurecom.fr>
2021-06-27 13:19:10 +0200

    all test ok

--- model_criticism_mmd/backends/kernels_torch/rbf_kernel.py
+++ model_criticism_mmd/backends/kernels_torch/rbf_kernel.py
@@ -107,106 +107,111 @@ class BasicRBFKernelFunction(BaseKernel):
 # NOTE: the code below seems to have issues (or bugs).
 # The learning process does not work at all.
 
-@nb.jit
-def func_distance(x, y, func_distance_metric) -> float:
-    d = func_distance_metric(x, y)
-    return d
 
-
-@nb.jit
-def compute_distance_matrix(x_1: np.ndarray, x_2: np.ndarray, func_distance_metric) -> np.ndarray:
-    d_matrix = np.empty(shape=(x_1.shape[0], x_2.shape[0]))
-    for i, x_sample in enumerate(x_1):
-        for j, y_sample in enumerate(x_2):
-            d_matrix[i, j] = func_distance(x_sample, y_sample, func_distance_metric)
-        # end for
-    # end for
-    return d_matrix
-
-
-class RBFCovariance(torch.autograd.Function):
-    """A function module which enables to auto-grad for Pytorch. forked from gpytorch package."""
-    @staticmethod
-    def forward(ctx, x1: torch.Tensor, x2: torch.Tensor, lengthscale, sq_dist_func, is_matrix_wise: bool = True):
-        """
-
-        Args:
-            ctx:
-            x1:
-            x2:
-            lengthscale:
-            sq_dist_func:
-            is_matrix_wise:
-
-        Returns:
-
-        """
-        dev = x1.device
-        dtype = x1.dtype
-        #if any(ctx.needs_input_grad[:2]):
-        #    raise RuntimeError("RBFCovariance cannot compute gradients with " "respect to x1 and x2")
-        if lengthscale.size(-1) > 1:
-            raise ValueError("RBFCovariance cannot handle multiple lengthscales")
-        needs_grad = any(ctx.needs_input_grad)
-        x1_ = x1.div(lengthscale)
-        x2_ = x2.div(lengthscale)
-
-        x1_np = x1_.cpu().detach().numpy()
-        x2_np = x2_.cpu().detach().numpy()
-        if is_matrix_wise:
-            unitless_sq_dist = sq_dist_func(x1_, x2_)
-        else:
-            unitless_sq_dist = compute_distance_matrix(x1_np, x2_np, sq_dist_func)
-        # end if
-        #d_torch = torch.Tensor(unitless_sq_dist).to(dev).type(dtype)
-        d_torch = unitless_sq_dist
-        # clone because inplace operations will mess with what's saved for backward
-        unitless_sq_dist_ = d_torch.clone() if needs_grad else d_torch
-        covar_mat = unitless_sq_dist_.div_(-2.).exp_()
-        if needs_grad:
-            d_output_d_input = unitless_sq_dist_.mul_(covar_mat).div_(lengthscale)
-            ctx.save_for_backward(d_output_d_input)
-        return covar_mat
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        d_output_d_input = ctx.saved_tensors[0]
-        lengthscale_grad = grad_output * d_output_d_input
-        return None, None, lengthscale_grad, None, None
-
-
-class AnyDistanceRBFKernelFunction(BasicRBFKernelFunction):
-    """You can use any distance metrics as your preference.
-    The distance metric must satisfy distance = distance_metric(x, y)"""
-
-    def __init__(self,
-                 func_distance_metric: typing.Callable[[np.ndarray, np.ndarray], float],
-                 device_obj: torch.device = device_default,
-                 log_sigma: Union[float, torch.Tensor] = 0.0,
-                 opt_sigma: bool = False):
-        """
-        Args:
-            func_distance_metric: a distance metric that satisfies distance = f(x, y).
-            device_obj: torch.device object.
-            log_sigma: sigma parameter of RBF kernel
-            opt_sigma: if True, then sigma parameter will be optimized during training. False, not.
-        """
-        super().__init__(device_obj=device_obj)
-        self.func_distance_metric = func_distance_metric
-        self.device_obj = device_obj
-        self.opt_sigma = opt_sigma
-        self.log_sigma = torch.tensor([log_sigma], requires_grad=opt_sigma, device=device_obj)
-        self.func_distance = RBFCovariance.apply
-
-    def compute_kernel_matrix(self, x: torch.Tensor, y: torch.Tensor, log_sigma: torch.Tensor = None,
-                              **kwargs) -> KernelMatrixObject:
-        if log_sigma is None:
-            log_sigma = self.log_sigma
-        # end if
-        sigma = torch.exp(log_sigma)
-        gamma = torch.div(1, (2 * torch.pow(sigma, 2)))
-        #k_xx = torch.exp(-1 * gamma * self.func_distance(x, x, torch.tensor([1.0]), self.func_distance_metric, True) ** 2)
-        k_xx = -1 * self.func_distance(x, x, torch.tensor([1.0]), self.func_distance_metric, True) ** 2
-        k_xy = torch.exp(-1 * gamma * self.func_distance(x, y, torch.tensor([1.0]), self.func_distance_metric, True) ** 2)
-        k_yy = torch.exp(-1 * gamma * self.func_distance(y, y, torch.tensor([1.0]), self.func_distance_metric, True) ** 2)
-        return KernelMatrixObject(k_xx=k_xx, k_yy=k_yy, k_xy=k_xy)
+# from gpytorch.kernels import RBFKernel
+# from gpytorch.kernels.rbf_kernel import postprocess_rbf
+# from gpytorch.kernels.kernel import Distance
+# from typing import Any
+#
+#
+# class CustomMetric(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx: Any, x1, x2) -> Any:
+#         v = torch.sum(x1 + x2)
+#         ctx.save_for_backward(v)
+#         return v
+#
+#     @staticmethod
+#     def backward(ctx, grad_outputs: Any) -> Any:
+#         return grad_outputs / ctx.saved_tensors[0], grad_outputs / ctx.saved_tensors[0]
+#
+#
+# class CustomDistance(Distance):
+#     def __init__(self, postprocess_script=postprocess_rbf, **kwargs):
+#         super().__init__()
+#         self._postprocess = postprocess_script
+#         self.metric = CustomMetric.apply
+#         self.metric_parameters = kwargs
+#
+#     def _sq_dist(self, x1, x2, postprocess, x1_eq_x2=False):
+#         # TODO: use torch squared cdist once implemented: https://github.com/pytorch/pytorch/pull/25799
+#         d_tensor = torch.zeros((x1.shape[0], x2.shape[0]))
+#
+#         for i_elem1, elem_1 in enumerate(x1):
+#             for j_elem2, elem_2 in enumerate(x2):
+#                 # v = torch.sum(elem_1 + elem_2)
+#                 # v = wasserstein_distance(elem_1, elem_2)
+#                 v = self.metric(elem_1, elem_2)
+#                 d_tensor[i_elem1, j_elem2] = v
+#         return self._postprocess(d_tensor) if postprocess else d_tensor
+#
+#         adjustment = x1.mean(-2, keepdim=True)
+#         x1 = x1 - adjustment
+#         x2 = x2 - adjustment  # x1 and x2 should be identical in all dims except -2 at this point
+#
+#         # Compute squared distance matrix using quadratic expansion
+#         x1_norm = x1.pow(2).sum(dim=-1, keepdim=True)
+#         x1_pad = torch.ones_like(x1_norm)
+#         if x1_eq_x2 and not x1.requires_grad and not x2.requires_grad:
+#             x2_norm, x2_pad = x1_norm, x1_pad
+#         else:
+#             x2_norm = x2.pow(2).sum(dim=-1, keepdim=True)
+#             x2_pad = torch.ones_like(x2_norm)
+#         x1_ = torch.cat([-2.0 * x1, x1_norm, x1_pad], dim=-1)
+#         x2_ = torch.cat([x2, x2_pad, x2_norm], dim=-1)
+#         res = x1_.matmul(x2_.transpose(-2, -1))
+#
+#         if x1_eq_x2 and not x1.requires_grad and not x2.requires_grad:
+#             res.diagonal(dim1=-2, dim2=-1).fill_(0)
+#
+#         # Zero out negative values
+#         res.clamp_min_(0)
+#         return self._postprocess(res) if postprocess else res
+#
+#     def _dist(self, x1, x2, postprocess, x1_eq_x2=False):
+#         # TODO: use torch cdist once implementation is improved: https://github.com/pytorch/pytorch/pull/25799
+#         res = self._sq_dist(x1, x2, postprocess=False, x1_eq_x2=x1_eq_x2)
+#         res = res.clamp_min_(1e-30).sqrt_()
+#         return self._postprocess(res) if postprocess else res
+#
+#
+# class AnyDistanceRBFKernelFunction(BasicRBFKernelFunction):
+#     """You can use any distance metrics as your preference.
+#     The distance metric must satisfy distance = distance_metric(x, y)"""
+#
+#     def __init__(self,
+#                  device_obj: torch.device = device_default,
+#                  log_sigma: Union[float, torch.Tensor] = 0.0,
+#                  opt_sigma: bool = False,
+#                  **kwargs):
+#         """
+#         Args:
+#             func_distance_metric: a distance metric that satisfies distance = f(x, y).
+#             device_obj: torch.device object.
+#             log_sigma: sigma parameter of RBF kernel
+#             opt_sigma: if True, then sigma parameter will be optimized during training. False, not.
+#         """
+#         super().__init__(device_obj=device_obj)
+#         self.device_obj = device_obj
+#         self.opt_sigma = opt_sigma
+#         self.log_sigma = torch.tensor([log_sigma], requires_grad=opt_sigma, device=device_obj)
+#         self.rbf_kernel_gpy = RBFKernel()
+#         self.rbf_kernel_gpy.distance_module = CustomDistance(**kwargs)
+#
+#     def compute_kernel_matrix(self, x: torch.Tensor, y: torch.Tensor, log_sigma: torch.Tensor = None,
+#                               **kwargs) -> KernelMatrixObject:
+#         if log_sigma is None:
+#             log_sigma = self.log_sigma
+#         # end if
+#         sigma = torch.exp(log_sigma)
+#         gamma = torch.div(1, (2 * torch.pow(sigma, 2)))
+#         is_matrix_wise = False
+#
+#         # todo need length scale off
+#         k_xx = self.rbf_kernel_gpy.forward(x, x)
+#         k_yy = self.rbf_kernel_gpy.forward(y, y)
+#         k_xy = self.rbf_kernel_gpy.forward(x, y)
+#         # k_xx = torch.exp(-1 * gamma * self.func_distance(x, x, torch.tensor([1.0]), self.func_distance_metric, is_matrix_wise) ** 2)
+#         # k_xy = torch.exp(-1 * gamma * self.func_distance(x, y, torch.tensor([1.0]), self.func_distance_metric, is_matrix_wise) ** 2)
+#         # k_yy = torch.exp(-1 * gamma * self.func_distance(y, y, torch.tensor([1.0]), self.func_distance_metric, is_matrix_wise) ** 2)
+#         return KernelMatrixObject(k_xx=k_xx, k_yy=k_yy, k_xy=k_xy)
