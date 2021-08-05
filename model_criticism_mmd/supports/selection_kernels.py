5f92ecea1dcbf696c8d960b448a14c45f3cc1f0a
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2021-08-05 16:45:55 +0200

    added a class to select the best kernel

---
+++ model_criticism_mmd/supports/selection_kernels.py
@@ -0,0 +1,63 @@
+from model_criticism_mmd.models.datasets import TwoSampleDataSet
+from model_criticism_mmd.backends.kernels_torch.base import BaseKernel
+from model_criticism_mmd.backends.backend_torch import ModelTrainerTorchBackend, MMD
+from model_criticism_mmd.logger_unit import logger
+
+import typing
+import torch
+
+
+class SelectionKernels(object):
+    def __init__(self,
+                 candidate_kernels: typing.List[typing.Tuple[torch.Tensor, BaseKernel]],
+                 dataset_validation: TwoSampleDataSet,
+                 device_obj: torch.device = torch.device('cpu'),
+                 is_training: bool = False,
+                 dataset_training: TwoSampleDataSet = None,
+                 num_epochs: int = 500,
+                 batchsize: int = 256):
+        """A class to select the best Kernel for the given dataset. The class selects the best kernel by test-power
+        which is represented "ratio" in the codes.
+
+        Args:
+            candidate_kernels: List object. Elements of lists are tuple (scale-vector, kernel object)
+            dataset_validation: a dataset object for validations.
+            device_obj: device object of Torch.
+            is_training: boolean. If True, then it runs optimization of parameters.
+            The initial value is the given parameter.
+            dataset_training: a dataset object which is required only when is_training = True.
+            num_epochs: #epochs only when is_training = True.
+            batchsize: batchsize only when is_training = True.
+        """
+        self.is_training = is_training
+        self.candidate_kernels = candidate_kernels
+        self.dataset_training = dataset_training
+        self.dataset_validation = dataset_validation
+        self.device_obj = device_obj
+        self.num_epochs = num_epochs
+        self.batchsize = batchsize
+
+    def run_selection(self) -> typing.List[typing.Tuple[BaseKernel, float]]:
+        results = []
+        for scale, kernel_obj in self.candidate_kernels:
+            mmd_estimator = MMD(kernel_function_obj=kernel_obj, scales=scale, device_obj=self.device_obj)
+            if self.is_training:
+                trainer_obj = ModelTrainerTorchBackend(mmd_estimator=mmd_estimator, device_obj=self.device_obj)
+                trained_params = trainer_obj.train(dataset_training=self.dataset_training,
+                                                   dataset_validation=self.dataset_validation,
+                                                   num_epochs=self.num_epochs,
+                                                   batchsize=self.batchsize)
+                trained_mmd_estimator = MMD(kernel_function_obj=trained_params.kernel_function_obj,
+                                            scales=torch.tensor(trained_params.scales),
+                                            device_obj=self.device_obj)
+                mmd_estimator = trained_mmd_estimator
+            # end if
+            val_x, val_y = self.dataset_validation.get_all_item()
+            mmd_result = mmd_estimator.mmd_distance(x=val_x, y=val_y)
+            test_power = mmd_result.ratio.detach().cpu().numpy()[0]
+            logger.info(f'Kernel-type: {kernel_obj} Ratio: {test_power}')
+            results.append((kernel_obj, test_power))
+        # end for
+        sorted_result = list(sorted(results, key=lambda t: t[1], reverse=True))
+        return sorted_result
+
---
+++ tests/supports/test_selection_kernels.py
@@ -0,0 +1,50 @@
+import torch
+
+from model_criticism_mmd.supports.selection_kernels import SelectionKernels
+from model_criticism_mmd.backends.kernels_torch import BasicRBFKernelFunction, MaternKernelFunction
+from model_criticism_mmd.models.datasets import TwoSampleDataSet
+
+import numpy as np
+
+
+def test_selection_kernels_without_training():
+    device_obj = torch.device('cpu')
+    x_train = np.random.normal(3, 0.5, size=(500, 2))
+    y_train = np.random.normal(3, 0.5, size=(500, 2))
+    dataset_train = TwoSampleDataSet(x_train, y_train, device_obj)
+
+    x_test = np.random.normal(3, 0.5, size=(100, 2))
+    y_test = np.random.normal(3, 0.5, size=(100, 2))
+    dataset_validation = TwoSampleDataSet(x_test, y_test, device_obj)
+
+    scales = torch.tensor([0.05, 0.05])
+    kernels = [(scales, BasicRBFKernelFunction(device_obj=device_obj)),
+               (scales, MaternKernelFunction(nu=0.5, device_obj=device_obj))]
+    selection_obj = SelectionKernels(candidate_kernels=kernels, dataset_validation=dataset_validation)
+    result_select = selection_obj.run_selection()
+
+
+def test_selection_kernels_with_training():
+    device_obj = torch.device('cpu')
+    x_train = np.random.normal(3, 0.5, size=(500, 2))
+    y_train = np.random.normal(3, 0.5, size=(500, 2))
+    dataset_train = TwoSampleDataSet(x_train, y_train, device_obj)
+
+    x_test = np.random.normal(3, 0.5, size=(100, 2))
+    y_test = np.random.normal(3, 0.5, size=(100, 2))
+    dataset_validation = TwoSampleDataSet(x_test, y_test, device_obj)
+
+    scales = torch.tensor([0.05, 0.05])
+    kernels = [(scales, BasicRBFKernelFunction(device_obj=device_obj)),
+               (scales, MaternKernelFunction(nu=0.5, device_obj=device_obj))]
+    selection_obj = SelectionKernels(candidate_kernels=kernels,
+                                     dataset_validation=dataset_validation,
+                                     dataset_training=dataset_train,
+                                     is_training=True,
+                                     num_epochs=250)
+    result_select = selection_obj.run_selection()
+
+
+if __name__ == '__main__':
+    test_selection_kernels_with_training()
+    test_selection_kernels_without_training()
